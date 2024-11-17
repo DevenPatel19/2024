6dff939de7c02d37446d93c5469353e9226fb7cf
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2024-11-16 21:20:16 -0500

    Keep on keeping on

---
+++ w01d2/linearSearch.js
@@ -0,0 +1,10 @@
+function linear_search(haystack, needle) {
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle) {
+            return true;
+        }
+    }
+    return false;
+}
+
+module.exports = linear_search;
